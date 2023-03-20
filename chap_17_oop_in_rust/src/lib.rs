ac54ed44f2ff6867725f3b4b5e94d59b3ecde923
Alexandre Baldé (WSL Win11 Pro) <alexandrer_b@outlook.com>
2023-03-20 03:26:20 +0000

    Add final content summarising chapter 17.3

--- chap_17_oop_in_rust/src/lib.rs
+++ chap_17_oop_in_rust/src/lib.rs
@@ -1,3 +1,15 @@
+/*
+IMPORTANT
+
+ |
+ | Dynamic dispatch can give your code some flexibility in exchange for a bit of
+ | runtime performance.
+ |
+ | You can use this flexibility to implement object-oriented patterns that can help your
+ | code’s maintainability.
+ |
+ */
+
 pub mod gui;
 pub mod blog_oop_st_obj;
 pub mod blog_enum;
